07a5fa2bd9c77526b14b59c4e0a47df75a187035
Danielaigm <danigarci852@gmail.com>
2023-03-23 14:58:50 -0500

    add class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,15 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        backgroundImage: {
+            carne: "url(src/assets/carne.png)",
+            hamburguesa: "url(src/assets/hamgurgesa.png)",
+            lechuga: "url(src/assets/lechuga.png)",
+            panUno: "url(src/assets/pan1.png)",
+            panDos: "url(src/assets/pan2.png)",
+            queso: "url(src/assets/queso.png)",
+            tocino: "url(src/assets/tocino.png)",
+        },
         extend: {},
     },
     plugins: [],
