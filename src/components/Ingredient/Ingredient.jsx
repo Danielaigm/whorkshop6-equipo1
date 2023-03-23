a26dfac24890c31010ede9b8d759aa378a2a7e80
dpaeza <danipaez1098@gmail.com>
2023-03-23 15:55:26 -0500

    change bread, meet and baicon layout

--- src/components/Ingredient/Ingredient.jsx
+++ src/components/Ingredient/Ingredient.jsx
@@ -5,9 +5,9 @@ const Ingredient = (props) => {
         case "bread-top":
             return (
                 <div className="bread-top">
-                    <div className="seeds1"></div>
+                    {/* <div className="seeds1"></div>
                     <div className="seeds2"></div>
-                    <div className="seeds1"></div>
+                    <div className="seeds1"></div> */}
                 </div>
             );
         default:
