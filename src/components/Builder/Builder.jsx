19fb548e9ef997b5e47a02aeca019beb5e78fe32
Danielaigm <danigarci852@gmail.com>
2023-03-23 19:18:42 -0500

    colors

--- src/components/Builder/Builder.jsx
+++ src/components/Builder/Builder.jsx
@@ -120,13 +120,13 @@ class Builder extends Component {
                     <div className=" flex  w-[500px] justify-between py-3">
                         <a
                             href="#_"
-                            className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
+                            className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-[#9d0208] transition duration-300 ease-out border-2 border-[#e85d04] rounded-full shadow-md group"
                             onClick={() => this.handleConfirm()}
                         >
-                            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#faa307] group-hover:translate-x-0 ease">
                                 <AiOutlineCheck className="text-2xl font-bold" />
                             </span>
-                            <span className="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+                            <span className="absolute flex items-center justify-center w-full h-full text-[#9d0208] transition-all duration-300 transform group-hover:translate-x-full ease">
                                 Confirm
                             </span>
                             <span className="relative invisible">
@@ -137,9 +137,9 @@ class Builder extends Component {
                         <Link to="/receipt">
                             <a
                                 href="#_"
-                                class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
+                                class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-[#9d0208] transition duration-300 ease-out border-2 border-[#e85d04] rounded-full shadow-md group"
                             >
-                                <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-[#faa307] group-hover:translate-x-0 ease">
                                     <svg
                                         class="w-6 h-6"
                                         fill="none"
@@ -155,7 +155,7 @@ class Builder extends Component {
                                         ></path>
                                     </svg>
                                 </span>
-                                <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+                                <span class="absolute flex items-center justify-center w-full h-full text-[#9d0208] transition-all duration-300 transform group-hover:translate-x-full ease">
                                     See receipt
                                 </span>
                                 <span class="relative invisible">
