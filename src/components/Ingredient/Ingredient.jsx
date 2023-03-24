ce5d8f010a4ee9eff65b387b39806707ffe3743d
Esteark <eardila1409@gmail.com>
2023-03-23 23:06:12 -0500

    Add corrections styles Builder

--- src/components/Ingredient/Ingredient.jsx
+++ src/components/Ingredient/Ingredient.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+import "./stylesIngredient.scss";
 
 const Ingredient = (props) => {
-    switch (props.id) {
-        case "bread-top":
-            return (
-                <div className="bread-top" >
-                    {/* <div className="seeds1"></div>
+  switch (props.id) {
+    case "bread-top":
+      return (
+        <div className="bread-top ingredient">
+          {/* <div className="seeds1"></div>
                     <div className="seeds2"></div>
                     <div className="seeds1"></div> */}
-                </div>
-            );
-        default:
-            return (
-                <div onClick={() => props.onClick()} className={props.id}></div>
-            );
-    }
+        </div>
+      );
+    default:
+      return (
+        <div
+          onClick={() => props.onClick()}
+          className={`ingredient ${props.id}`}
+        ></div>
+      );
+  }
 };
 
 export default Ingredient;
